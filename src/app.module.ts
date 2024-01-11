be451e13a7e118a27360b0a23f527148cc831e8c
eduardo0915 <eduardomartinezramirez0215@gmail.com>
2024-01-11 14:50:25 -0400

    cambios en la bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,9 @@ import { JoiValidationSchema } from './common/config/joi.validation';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(
+      'mongodb+srv://eduardo:elm141595@cluster0.61bwl5d.mongodb.net/nest-pokemon',
+    ),
     PokemonModule,
     CommonModule,
     SeedModule,
